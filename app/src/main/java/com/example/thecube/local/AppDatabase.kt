f14feda60577395f822ea58424de52c6b5e3ea3f
Rotems00 <rotem11ziv11@gmail.com>
2025-03-06 22:16:42 +0200

    completed functionality of adding a dish,with pic to firebase db and room

--- app/src/main/java/com/example/thecube/local/AppDatabase.kt
+++ app/src/main/java/com/example/thecube/local/AppDatabase.kt
@@ -2,26 +2,29 @@ package com.example.thecube.local
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.thecube.model.Dish
 import kotlin.concurrent.Volatile
 
 
-@Database(entities = [Dish::class], version =1, exportSchema = false)
-abstract class AppDatabase:RoomDatabase(){
+@Database(entities = [Dish::class], version = 2, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
     abstract fun dishDao(): DishDao
 
-    companion object{
+    companion object {
         @Volatile
-        private var INSTANCE: AppDatabase?=null
+        private var INSTANCE: AppDatabase? = null
 
-        fun getDatabase(context:Context):AppDatabase{
-            return INSTANCE?: synchronized(this){
-                val instance = androidx.room.Room.databaseBuilder(
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
                     "app_database"
-                ).build()
+                )
+                    .fallbackToDestructiveMigration() // This is the simple solution
+                    .build()
                 INSTANCE = instance
                 instance
             }
