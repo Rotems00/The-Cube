a8c450e1040fd6314ca114c5c14d44660f9cd63a
ShonHason <ShonHason4@gmail.com>
2025-03-29 17:05:45 +0300

    finish logic and style

--- app/src/main/java/com/example/thecube/MyApplication.kt
+++ app/src/main/java/com/example/thecube/MyApplication.kt
@@ -1,7 +1,5 @@
 package com.example.thecube
 
-
-
 import android.app.Application
 import com.cloudinary.android.MediaManager
 
@@ -9,7 +7,11 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        // Cloudinary configuration
+        // Initialize Cloudinary configuration
+        initCloudinary()
+    }
+
+    private fun initCloudinary() {
         val config = mapOf(
             "cloud_name" to "dlqydpa1y",
             "api_key" to "686692495789422",
@@ -17,7 +19,7 @@ class MyApplication : Application() {
             "secure" to true
         )
 
-        // Initialize Cloudinary's MediaManager with the configuration
+        // Initialize Cloudinary's MediaManager
         MediaManager.init(this, config)
     }
 }
